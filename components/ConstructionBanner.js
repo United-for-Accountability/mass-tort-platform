a737b971102a764d0575fcc8bfa7d6716543b859
Vinnie <85443832+vincecordova@users.noreply.github.com>
2025-07-30 09:21:17 -0700

    Add site construction banner

---
+++ components/ConstructionBanner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ConstructionBanner() {
+  return (
+    <div className="bg-yellow-300 text-center py-2 text-sm font-semibold text-gray-800">
+      Site is under construction - will be ready soon
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css';
 import AIAssistant from '../components/AIAssistant';
+import ConstructionBanner from '../components/ConstructionBanner';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <ConstructionBanner />
       <Component {...pageProps} />
       <AIAssistant />
     </>
