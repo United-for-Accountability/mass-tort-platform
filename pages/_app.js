fb17c0edda33b68596297d6cefa288386c2602ad
Vinnie <85443832+vincecordova@users.noreply.github.com>
2025-08-02 16:15:50 -0700

    Add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import AIAssistant from '../components/AIAssistant';
 import ConstructionBanner from '../components/ConstructionBanner';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
       <ConstructionBanner />
       <Component {...pageProps} />
       <AIAssistant />
+      <Analytics />
     </>
   );
 }
