08ee2bb6ac5238f28c1beaa2bf2e7ab651a6a87b
Vinnie <85443832+vincecordova@users.noreply.github.com>
2025-08-02 13:57:57 -0700

    Update declaration.js

--- pages/cases/unauthorized-rule/declaration.js
+++ pages/cases/unauthorized-rule/declaration.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Navbar from '../../../components/Navbar';
+import Footer from '../../../components/Footer';
 
 export default function DeclarationForm() {
   return (
